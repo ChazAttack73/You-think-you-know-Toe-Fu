0c4e4ba9600b9849e6f77cb3f061b8e56f44edea
Chaz <cmtlum73@gmail.com>
2016-01-23 12:00:21 -1000

    adds autoloader for assets

--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -7,7 +7,20 @@ ToeFu.Boot = function () {
 
 };
 
-ToeFu.Boot.prototype.preload = function () {
+ToeFu.Boot.prototype.preload = function (){
+
+  //autoload each asset by type
+  Object.keys( ToeFu.ASSETS ).forEach( function ( type ){
+    for ( var asset in ToeFu.ASSETS[ type ] ){
+      ToeFu.game.load[ type.toLowerCase() ](
+        ToeFu.ASSETS[ type ][ asset ].name,
+        ToeFu.ASSETS[ type ][ asset ].path,
+        ToeFu.ASSETS[ type ][ asset ].width,
+        ToeFu.ASSETS[ type ][ asset ].height,
+        ToeFu.ASSETS[ type ][ asset ].frames
+      );
+    }
+  });
 
 };
 
