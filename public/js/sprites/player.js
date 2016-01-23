95d0e2a67b5acb93df426cda3ce001a8d15e18d5
Chaz <cmtlum73@gmail.com>
2016-01-23 12:40:06 -1000

    adds idle animation

--- public/js/sprites/player.js
+++ public/js/sprites/player.js
@@ -1,13 +1,28 @@
-ToeFu.Player = function ( game, id, name ) {
-  this.game = game,
-  this.id = id,
-  this.name = name? name : 'Player ' + (id+1);
+(function () {
 
-  Phaser.Sprite.call( this, game, 0, 0, ToeFu.ASSETS.SPRITESHEET.PLAYER.name );
-};
+  var ANIMATIONS = {
+    IDLE : {
+      name : 'idle',
+      frames : [0,1,2,3],
+      fps : 5
+    }
+  };
 
-ToeFu.Player.prototype = Object.create( Phaser.Sprite.prototype, {
-  constructor : {
-    value : ToeFu.Player
-  }
-});
+  ToeFu.Player = function ( game, id, name ) {
+    this.game = game,
+    this.id = id,
+    this.name = name? name : 'Player ' + (id+1);
+
+    Phaser.Sprite.call( this, game, 0, 0, ToeFu.ASSETS.SPRITESHEET.PLAYER.name );
+
+    this.animations.add( ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.frames );
+    this.animations.play( ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.fps, true );
+
+  };
+
+  ToeFu.Player.prototype = Object.create( Phaser.Sprite.prototype, {
+    constructor : {
+      value : ToeFu.Player
+    }
+  });
+})();
--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -21,11 +21,9 @@ ToeFu.Boot.prototype.preload = function (){
       );
     }
   });
-
 };
 
 ToeFu.Boot.prototype.create = function () {
   // switch to game state
   this.state.start( ToeFu.STATES.GAME );
-
 };
